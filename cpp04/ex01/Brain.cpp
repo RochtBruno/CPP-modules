633015027a8abaca07b1217034943f64b2c7b00d
RochtBruno <bruno.taveira.r0.382@gmail.com>
2025-02-24 16:56:17 -0300

    started ex01 cpp04

---
+++ cpp04/ex01/Brain.cpp
@@ -0,0 +1,31 @@
+#include "Brain.hpp"
+
+std::string getIdeas(){
+	
+}
+
+Brain::Brain()
+{
+	std::cout << "Brain constructor called" << std::endl;
+}
+
+Brain::~Brain()
+{
+	std::cout << "Brain destructor called" << std::endl;
+}
+
+Brain::Brain(const Brain &brain)
+{
+	std::cout << "Brain copy constructor called" << std::endl;
+	*this = brain;
+}
+
+Brain &Brain::operator=(const Brain &brain)
+{
+	std::cout << "Brain assignation operator called" << std::endl;
+	for (int i = 0; i < 100; i++)
+		this->ideas[i] = brain.ideas[i];
+	return *this;
+}
+
+
